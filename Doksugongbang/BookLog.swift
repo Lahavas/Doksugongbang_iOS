1f9cb0a2e7fabaf1dbdf7fd8ca81a29e8c752417
lahavas <hos6959@gmail.com>
2017-08-20 16:42:17 +0900

    Create BookLog Model

---
+++ Doksugongbang/BookLog.swift
@@ -0,0 +1,34 @@
+//
+//  BookLog.swift
+//  Doksugongbang
+//
+//  Created by Yeon on 2017. 8. 20..
+//  Copyright © 2017년 yeon. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class BookLog: Object {
+    
+    // MARK: - Properties
+    
+    dynamic var bookLogId: String = UUID().uuidString
+    
+    dynamic var startPage: Int = 0
+    dynamic var endPage: Int = 0
+    
+    dynamic var logContent: String = ""
+    
+    dynamic var dateUpdated: Date = Date()
+    
+    let bookLogs = List<BookLog>()
+    let bookInfos = LinkingObjects(fromType: BookInfo.self, property: "bookLogs")
+    
+    // MARK: - Methods
+    
+    override static func primaryKey() -> String? {
+        return "bookLogId"
+    }
+}
+
